8605f7ffd5fe97ee4af913a0f14519ea34c88788
Success <successsimeon484@gmail.com>
2026-04-10 08:01:16 +0100

    feat: updating

--- main_project/settings.py
+++ main_project/settings.py
@@ -121,7 +121,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
