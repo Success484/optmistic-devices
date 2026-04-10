52450e33e574f33fd3f08ff9d9adb85ebc9814f7
Success <successsimeon484@gmail.com>
2026-04-10 08:44:04 +0100

    feat: updating

--- main_project/settings.py
+++ main_project/settings.py
@@ -117,8 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# IMPORTANT: only if your static folder is here:
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 
 import os
